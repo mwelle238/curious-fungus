517b8483079dd7fdbb1c90ea3f52048bec144f07
Michael Welle <michaelpwelle@gmail.com>
2023-05-04 18:36:24 -0700

    stupid bugfix that took way too long to figure out

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,12 +13,12 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content, id) => {
+export const putDb = async (content) => {
   console.log("Put data in the database");
   const jateDb = await openDB('jate', 1);
   const tx = jateDb.transaction('jate', 'readwrite');
   const store = tx.objectStore('jate');
-  const request = store.put(content, id);
+  const request = store.put({ id: 1, value: content});
   const result = await request;
   console.log('data saved to the database', result);
 }
@@ -29,7 +29,7 @@ export const getDb = async () => {
   const jateDb = await openDB('jate', 1);
   const tx = jateDb.transaction('jate', 'readonly');
   const store = tx.objectStore('jate');
-  const request = store.getAll();
+  const request = store.get(1);
   const result = await request;
   console.log('result.value', result);
   return result;
